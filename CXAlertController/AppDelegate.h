3cfbafe6ba41bacae612b536d8d51ca1cd6146cb
ziacke <602637103@qq.com>
2015-12-23 16:12:23 +0800

    Initial Commit

---
+++ CXAlertController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CXAlertController
+//
+//  Created by sunkuo on 15/12/23.
+//  Copyright © 2015年 chuxinnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CXAlertController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CXAlertController
+//
+//  Created by sunkuo on 15/12/23.
+//  Copyright © 2015年 chuxinnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
